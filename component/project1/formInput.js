b7f98867eb81473c64fbac62bdf720e18a44488a
fightism <fightism@gmail.com>
2022-11-21 00:52:43 +0700

    Update formInput.js

--- component/project1/formInput.js
+++ component/project1/formInput.js
@@ -1,4 +1,4 @@
-import Row from "react-bootstrap/Row";
+cimport Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import { MdAddCircleOutline } from "react-icons/md";
